7eaee3400f25139d96612a967b34bf2b77357bb8
hpcjkour <hpcjkour@hlrs.de>
2018-05-07 14:56:55 +0200

    Correct Typo

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [metasoarus/oz "1.3.1"]])
+                 [metasoarous/oz "1.3.1"]])
